6e740ab64c3200d2c6dcf995a93184dfdaf498d4
Filipe Piccoli <filipe_mqt@hotmail.com>
2018-10-19 19:35:35 -0300

    layout de cartas dinamico

--- js/teste.js
+++ js/teste.js
@@ -4,79 +4,38 @@ const jsonfile = require('jsonfile-promised');
 const fs = require('fs');
 var heroes;
 
-getJSON('http://gdurl.com/KM4u'); //heroes
-//var cards = fetch('http://gdurl.com/dAE9'); //cards
+//getJSON('http://gdurl.com/KM4u'); //heroes
+getJSON('http://gdurl.com/dAE9', 2); //cards
 
-function getJSON(url){
+function getJSON(url, codigo){
   fetch(url).then(function(response) {
     response.text().then(function(text) {
-      heroes = JSON.parse(text);
+      retorno = JSON.parse(text);
+
+console.log(retorno);
+
+let stringHTML = '<div class="col-lg-12">';
+
+for(let i in retorno.Decks[codigo].Cards){
+console.log('i: ',i);
+console.log('i%5: ',i%5);
+  if(i==0){
+    console.log('if1');
+    stringHTML += '<div class="row"><div class="col-lg-1"></div>';
+  }
+  else if (i%5 == 0) {
+    console.log('elseif');
+    stringHTML += '<div class="col-lg-1"></div></div><div class="row"><div class="col-lg-1"></div>';
+  }
+
+  console.log('addLink');
+  stringHTML += '<div class="col-lg-2"><img src="'+retorno.Decks[codigo].Cards[i].imgURL+'" height="100%" width="100%"></div>';
+}
+  console.log('fechaArquivo');
+stringHTML += '</div></div>';
+
+console.log(stringHTML);
+panelBody.innerHTML = stringHTML;
     });
   });
 };
-
-panelBody.innerHTML = '<div class="col-lg-12">'+
-
-                            '<div class="row">'+
-
-                                '<div class="col-lg-1">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/FgOP" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/mqNM" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/bvmc" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/0Bi2" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/ONCg" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-1">'+
-                                '</div>'+
-                            '</div>'+
-
-                            '<div class="row">'+
-                                '<div class="col-lg-1">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/gBNo" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/DS6e" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/BG6I" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/Blzx" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/lUeN" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-1">'+
-                                '</div>'+
-                            '</div>'+
-
-                            '<div class="row">'+
-                                '<div class="col-lg-1">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/RtqC" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/5EBZ" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/hIzs" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-2">'+
-                                    '<img src="http://gdurl.com/Osif" height="100%" width="100%">'+
-                                '</div>'+
-                                '<div class="col-lg-1">'+
-                                '</div>'+
-                            '</div>';
