667d6b2803e30721fe462404c86330e366ea5c75
Filipe Piccoli <filipe_mqt@hotmail.com>
2019-02-08 19:38:40 -0200

    teste de conexao

--- js/mongo-data.js
+++ js/mongo-data.js
@@ -1,11 +1,23 @@
 var mongojs = require('mongojs');
-var db = mongojs('mongodb://readonly:txcread@tymbox-shard-00-00-8gad9.mongodb.net:27017,tymbox-shard-00-01-8gad9.mongodb.net:27017,tymbox-shard-00-02-8gad9.mongodb.net:27017/test?ssl=true&replicaSet=tymbox-shard-0&authSource=admin&retryWrites=true');
+
+var user = 'readwrite';
+var pass = 'txcreadwrite';
+var db = 'deckcreator';
+var url = 'mongodb://'+user+':'+pass+'@tymbox-shard-00-00-8gad9.mongodb.net:27017,tymbox-shard-00-01-8gad9.mongodb.net:27017,tymbox-shard-00-02-8gad9.mongodb.net:27017/'+db+'?ssl=true&replicaSet=tymbox-shard-0&authSource=admin&retryWrites=true';
+
+var collection = 'cards';
+
+// var url = '127.0.0.1/teste'
+// var collection = 'dados'
+
+var db = mongojs(url, [collection]);
+
 
 module.exports = {
   listAll(){
-    var cards = db.collection('cards');
-    cards.find({name: "Lich"}, function (err, docs) {
-      return docs;
+    db.cards.find(function (err, docs) {
+      console.log(docs);
+      // return docs;
     })
   }
 }
