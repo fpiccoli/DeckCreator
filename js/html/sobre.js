5755d6c9e0d8bd191b0b6be07eecd034aaed2ad5
fpiccoli <filipe_mqt@hotmail.com>
2022-06-06 17:10:32 -0300

    patch notes

--- js/html/sobre.js
+++ js/html/sobre.js
@@ -4,9 +4,14 @@ var moment = require('moment');
 
 module.exports = { html }
 
-function html(lista, game){
+function html(lista, game) {
   let property = 'changes';
-  if(game === "M&D") property = 'funcionalidades';
+  moment.locale('en');
+
+  if (game === "M&D") {
+    moment.locale('pt-br');
+    property = 'funcionalidades';
+  }
 
   let text = [];
 
@@ -15,11 +20,11 @@ function html(lista, game){
 
   lista = dataManager.dateSort(lista, 0);
 
-  lista.forEach(function (objeto, index, array){
+  lista.forEach(function (objeto, index, array) {
     text.push(h3(objeto.versao));
     text.push(h4(moment(objeto.data, "DD/MM/YYYY").format("LL")));
 
-    objeto[property].forEach(function (objeto, index, array){
+    objeto[property].forEach(function (objeto, index, array) {
       text.push(patch([
         {
           titulo: objeto.titulo,
@@ -29,52 +34,52 @@ function html(lista, game){
     });
   });
 
-  let json = builder.element('div', {class:'col-lg-12'}, text)
+  let json = builder.element('div', { class: 'col-lg-12' }, text)
 
   return builder.build([json]);
 }
 
-function patch(lista){
+function patch(lista) {
   let retorno = []
-  lista.forEach(function (objeto, index, array){
-    retorno.push({texto: builder.text(objeto.titulo), items:ul(objeto.itens)});
+  lista.forEach(function (objeto, index, array) {
+    retorno.push({ texto: builder.text(objeto.titulo), items: ul(objeto.itens) });
   });
   return ul2(retorno);
 }
 
-function p(string){
+function p(string) {
   return builder.element('p', null, [builder.text(string)]);
 }
 
-function h1(string){
-  return builder.element('h1', {class:'page-header'}, [builder.text(string)]);
+function h1(string) {
+  return builder.element('h1', { class: 'page-header' }, [builder.text(string)]);
 }
 
-function h2(string){
+function h2(string) {
   return builder.element('h2', null, [builder.text(string)]);
 }
 
-function h3(string){
+function h3(string) {
   return builder.element('h3', null, [builder.text(string)]);
 }
 
-function h4(string){
+function h4(string) {
   return builder.element('h4', null, [builder.text(string)]);
 }
 
-function ul(lista, tipo){
+function ul(lista, tipo) {
   let li = [];
   lista.forEach(monta);
-  function monta(item, index, array){
+  function monta(item, index, array) {
     li.push(builder.element('li', null, [builder.text(item)]));
   }
   return builder.element('ul', null, li);
 }
 
-function ul2(lista){
+function ul2(lista) {
   let li = [];
   lista.forEach(monta);
-  function monta(item, index, array){
+  function monta(item, index, array) {
     li.push(builder.element('li', null, [item.texto, item.items]));
   }
   return builder.element('ul', null, li);
